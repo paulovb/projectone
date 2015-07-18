34f127cafb6177385045cebd9dd7b8aaf55c84e4
Lucas Ortigara Reis <luucasor@gmail.com>
2015-07-18 17:43:26 -0300

    Teste da Excessão Valor Inválido!

--- src/main/java/CaixaEletronico.java
+++ src/main/java/CaixaEletronico.java
@@ -1,21 +1,51 @@
 public class CaixaEletronico {
 
-    public static int saque(int valor){
-        int retorno = 0;
-        if(valor >= 100){
-            retorno = valor/100;
-        } else {
-            if (valor >= 50){
-                retorno = valor/50;
+    public enum Notas {
+        CEM(100),
+        CINQUENTA(50),
+        VINTE(20),
+        DEZ(10);
+
+        public int nota;
+
+        Notas(int nota) {
+            this.nota = nota;
+        }
+    }
+
+    private static int[][] notas = {};
+
+    private static void adicionaNota(Notas nota){
+        notas[nota.ordinal()][1] = notas[nota.ordinal()][1] + 1;
+    }
+
+    public static int[][] saque(int valor) throws Exception{
+
+        notas = new int[][]{{100, 0}, {50, 0}, {20, 0}, {10, 0}};
+
+        while (valor > 0) {
+
+            if (valor >= Notas.CEM.nota) {
+                adicionaNota(Notas.CEM);
+                valor = valor - Notas.CEM.nota;
+
+            } else if (valor >= Notas.CINQUENTA.nota) {
+                adicionaNota(Notas.CINQUENTA);
+                valor = valor - Notas.CINQUENTA.nota;
+
+            } else if (valor >= Notas.VINTE.nota) {
+                adicionaNota(Notas.VINTE);
+                valor = valor - Notas.VINTE.nota;
+
+            } else if (valor >= Notas.DEZ.nota){
+                adicionaNota(Notas.DEZ);
+                valor = valor - Notas.DEZ.nota;
             } else {
-                if(valor >= 20){
-                    retorno = valor/20;
-                } else {
-                    retorno = valor/10;
-                }
+                throw new Exception("Valor inválido para saque!");
             }
+
         }
 
-        return retorno;
+        return notas;
     }
 }
--- src/test/java/CaixaEletronicoTest.java
+++ src/test/java/CaixaEletronicoTest.java
@@ -4,22 +4,55 @@ import org.junit.Test;
 public class CaixaEletronicoTest {
 
     @Test
-    public void saqueDe300Test(){
-        Assert.assertEquals(CaixaEletronico.saque(300),3);
+    public void saqueDe300Test() throws Exception{
+        int[][] esperado = {{100,3},{50,0},{20,0},{10,0}};
+        int[][] resultado = CaixaEletronico.saque(300);
+        Assert.assertArrayEquals(esperado, resultado);
     }
 
     @Test
-    public void saqueDe50Test(){
-        Assert.assertEquals(CaixaEletronico.saque(50),1);
+    public void saqueDe50Test() throws Exception{
+        int[][] resultado = {{100,0},{50,1},{20,0},{10,0}};
+        Assert.assertArrayEquals(CaixaEletronico.saque(50),resultado);
     }
 
     @Test
-    public void saqueDe20Test(){
-        Assert.assertEquals(CaixaEletronico.saque(20),1);
+    public void saqueDe20Test() throws Exception{
+        int[][] resultado = {{100,0},{50,0},{20,1},{10,0}};
+        Assert.assertArrayEquals(CaixaEletronico.saque(20),resultado);
     }
 
     @Test
-    public void saqueDe10Test(){
-        Assert.assertEquals(CaixaEletronico.saque(10),1);
+    public void saqueDe10Test() throws Exception{
+        int[][] resultado = {{100,0},{50,0},{20,0},{10,1}};
+        Assert.assertArrayEquals(CaixaEletronico.saque(10),resultado);
     }
+
+    @Test
+    public void saqueDe30Test() throws Exception{
+
+        int[][] resultado = {{100,0},{50,0},{20,1},{10,1}};
+        Assert.assertArrayEquals(CaixaEletronico.saque(30), resultado);
+    }
+
+    @Test
+    public void saqueDe350Test() throws Exception{
+
+        int[][] resultado = {{100,3},{50,1},{20,0},{10,0}};
+        Assert.assertArrayEquals(CaixaEletronico.saque(350), resultado);
+    }
+
+    @Test
+    public void saqueDe305Test() throws Exception{
+
+        try {
+            CaixaEletronico.saque(305);
+        } catch (Exception e){
+            Assert.assertTrue(true);
+            return;
+        }
+        Assert.fail();
+    }
+
+
 }
